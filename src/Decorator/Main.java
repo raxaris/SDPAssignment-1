590eba2bbdd6a15aefa13a0be1001e47add66576
raxar <ansar.utenov@gmail.com>
2023-10-08 19:45:29 +0600

    Decorator Patter with Application

---
+++ src/Decorator/Main.java
@@ -0,0 +1,12 @@
+package Decorator;
+
+import Decorator.Engine.*;
+import Decorator.EngineComponents.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleApplication app = new ConsoleApplication();
+        app.start();
+    }
+}
--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        IEngine customEngine = new ECUDecorator(new ExhaustSystemDecorator(new TurboChargerDecorator(new InjectionSystemDecorator(new VTypeEngine(Engine.EngineType.Diesel)))));
-        System.out.println(customEngine.getDesctiption());
-    }
-}
